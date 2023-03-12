52ce0dd4ed93f87cc7053b920fd8586020bfc740
Şəbnəm <mmmdovasbnm47@gmail.com>
2023-03-12 20:26:14 +0400

    first commit

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task 1
+int num = 54321;
+int reverse = 0;
+
+
+
+while (num != 0)
+{
+    reverse = reverse * 10;
+    reverse = reverse + num % 10;
+    num = num / 10;
+}
+Console.WriteLine(reverse);
+
